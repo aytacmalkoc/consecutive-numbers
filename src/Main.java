6cfbd8a6fc301332432ffff57e5e7a614c5fc9df
Aytac Malkoc <iletisim@aytacmalkoc.com.tr>
2020-05-11 13:39:07 +0300

    Java Ardışık Sayılar

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        int baslangic, bitis, toplam = 0;
+
+        System.out.print("Başlangıç Sayısını Giriniz: ");
+        baslangic = scanner.nextInt();
+        System.out.print("Bitiş Sayısını Giriniz: ");
+        bitis = scanner.nextInt();
+
+
+        int[] sayilar = new int[bitis];
+
+        for (int i = baslangic; i < bitis; i++){
+            sayilar[i] = i;
+            toplam += i;
+        }
+
+        System.out.println("Girilen sayıların arasındaki sayıların toplamı : " + toplam);
+
+    }
+}
