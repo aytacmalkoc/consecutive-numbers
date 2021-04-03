0319ffcc2dea7edb26d9376ed3def5f3065e602a
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2021-04-03 13:52:35 +0300

    code readability improved

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
 
-        Scanner scanner = new Scanner(System.in);
-        int baslangic, bitis, toplam = 0;
+    public static Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Başlangıç Sayısını Giriniz: ");
-        baslangic = scanner.nextInt();
-        System.out.print("Bitiş Sayısını Giriniz: ");
-        bitis = scanner.nextInt();
+    public static void main(String[] args)
+    {
+        int starts, end;
 
+        System.out.print("Number of Starts: ");
+        starts = scanner.nextInt();
+        System.out.print("Number of Finishes: ");
+        end = scanner.nextInt();
 
-        int[] sayilar = new int[bitis];
+        System.out.println("Sum of consecutive numbers: " + sum(starts, end));
+    }
 
-        for (int i = baslangic; i < bitis; i++){
-            sayilar[i] = i;
-            toplam += i;
+    public static int sum(int starts, int end)
+    {
+        int total = 0;
+
+        for (int i = starts; i < end; i++){
+            total += i;
         }
 
-        System.out.println("Girilen sayıların arasındaki sayıların toplamı : " + toplam);
-
+        return total;
     }
 }
